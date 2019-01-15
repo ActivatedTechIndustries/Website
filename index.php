ad82de7e2fd081f9d6ab420a00702c584283ea76
Gabriel Fontes <gabrielaofontes@gmail.com>
2019-01-15 09:35:32 +0000

    Removed Timer - Due to Errors

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
-	<!--<?php include 'timer.php';?>-->
+	<?php //include 'timer.php';?>
 	<?php include 'random_functions.php';?>
 	
 	<script>
