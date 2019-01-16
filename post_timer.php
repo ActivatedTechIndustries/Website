31674eb8de989dfb73c402ae6b37c47964847b4a
Gabriel Fontes <gabrielaofontes@gmail.com>
2019-01-16 11:38:42 +0000

    Added Timer Function with Post

---
+++ post_timer.php
@@ -0,0 +1,54 @@
+<?php
+		
+		if(isset($_POST["Total_View"]) || isset($_POST["Parsed_View"]))
+		{
+				if(isset($_POST["Parsed_View"]))
+				{
+			
+					$sql = "UPDATE counters_mikla SET counter=counter+1 WHERE text='Parsed_Number_Views' ";
+					
+					//echo $sql;exit();
+					
+					//Executar a ordem em $sql
+					$resultado = mysqli_query($ligacao,$sql);
+						
+					//echo $sql;exit();
+					
+					if(!$resultado)
+					{
+					
+						echo"
+							alert('Não foi possível ligar á base de dados (Ordem não executada)');";
+					}
+					else
+					{
+						echo"
+							alert('Ordem Executada com Sucesso');";
+					}
+					
+					//var_dump($_SESSION);exit();
+				}
+				
+				$sql = "UPDATE counters_mikla SET counter=counter+1 WHERE text='Total_Number_Views' ";
+						
+				//echo $sql;exit();
+						
+				//Executar a ordem em $sql
+				$resultado = mysqli_query($ligacao,$sql);
+							
+				//echo $sql;exit();
+						
+				if(!$resultado)
+				{
+						
+					echo"
+						alert('Não foi possível ligar á base de dados Ordem não executada');";
+				}
+				else
+				{
+					echo"
+						alert('Ordem Executada com Sucesso');";
+				}
+		}
+		
+?>
