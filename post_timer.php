ed736b1c38deb2cec72026b0f16d135e8ff9df7a
Friendly Doctor <46681973+FriendlyDoctor@users.noreply.github.com>
2019-01-17 09:37:39 +0000

    Updated post timer File, now works as intended
    Made by F0ntes and the Wizard of a Teacher

--- post_timer.php
+++ post_timer.php
@@ -1,9 +1,14 @@
 <?php
-			
+		
 		require "start_db.php";
-
+		
+		var_dump($_POST);
+		
+		//isset($_POST["Total_View"]) || isset($_POST["Parsed_View"])
+		
 		if(isset($_POST["Total_View"]) || isset($_POST["Parsed_View"]))
 		{
+				//isset($_POST["Parsed_View"])
 				if(isset($_POST["Parsed_View"]))
 				{
 			
@@ -20,7 +25,7 @@
 					{
 					
 						echo"
-							alert('Não foi possível ligar á base de dados (Ordem não executada)');";
+							alert('NÃ£o foi possÃ­vel ligar Ã¡ base de dados (Ordem nÃ£o executada)');";
 					}
 					else
 					{
@@ -31,7 +36,7 @@
 					//var_dump($_SESSION);exit();
 				}
 				
-				$sql = "UPDATE counters_mikla SET counter=counter+1 WHERE text='Total_Number_Views' ";
+				$sql = "UPDATE counters_mikla SET counter=counter+1 WHERE text='Total_Number_Views'";
 						
 				//echo $sql;exit();
 						
@@ -44,10 +49,11 @@
 				{
 						
 					echo"
-						alert('Não foi possível ligar á base de dados Ordem não executada');";
+						alert('NÃ£o foi possÃ­vel ligar Ã¡ base de dados Ordem nÃ£o executada');";
 				}
 				else
 				{
+					$_SESSION["view"] = 1;
 					echo"
 						alert('Ordem Executada com Sucesso');";
 				}
