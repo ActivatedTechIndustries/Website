40ed628da396c2391a4d63570a19fec13365d2ab
Gabriel Fontes <gabrielaofontes@gmail.com>
2019-01-26 14:39:35 +0000

    Update start_db.php - 404 Page Added

--- start_db.php
+++ start_db.php
@@ -6,8 +6,8 @@
 		{
 			echo"
 			<script>
-				alert('Não foi possivel ligar ao servidor');
-				window.location.assign('index.php')
+				/*alert('Não foi possivel ligar ao servidor');*/
+				window.location.assign('404page/404.html')
 				</script>
 				";
 				exit();
