4af3d5d6380c358e8cf5d90fa02b78a9e9ac81d6
ILoveMeAndYourMother <46692389+ILoveMeAndYourMother@users.noreply.github.com>
2019-01-30 22:33:39 +0000

    Add files via upload

---
+++ AboutUs.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<!-- HTML Page -->
+    <title>Activated Tech Industries</title>
+    <link rel="icon" type="image/png" href="imagens/favicon_logo.png" />
+	<!-- HTML Page -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link  href="style.css" rel="stylesheet">
+	<link  href="style_gradient.css" rel="stylesheet">
+	<link  href="scrollbar.css" rel="stylesheet">
+	<!--font do texto-->
+	<link href="https://fonts.googleapis.com/css?family=Nova+Mono" rel="stylesheet">
+	<!-------------------->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body id="body1"  style="max-width:100%;overflow-x:hidden; background: rgb(27,35,45);background: -moz-linear-gradient(top, rgba(27,35,45,1) 64%, rgba(27,35,45,1) 85%);background: -webkit-linear-gradient(top, rgba(27,35,45,1) 64%,rgba(27,35,45,1) 85%);background: linear-gradient(to bottom, rgba(27,35,45,1) 64%,rgba(27,35,45,1) 85%);filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1b232d', endColorstr='#1b232d',GradientType=0 );" onload="Page_View_Counter()">
+		<nav id="nav1" class="navbar navbar-expand-md navbar-dark " style="background-color: #333;max-height:9vh;">
+           
+		   <!-- Brand/logo -->
+            <a class="navbar-brand "  href="index.php" style="color: white">
+                <img src="imagens/logo_white.png" alt="Logo" style="width: 40px;cursor:pointer;">
+            </a>
+            <a class="navbar-brand d-none d-sm-block" href="index.php" style="color: white;cursor:pointer;">Activated Tech Industries</a>
+			<a class="navbar-brand d-block d-sm-none" href="index.php" style="color: white;font-size:medium; cursor:pointer;">Activated Tech Industries</a>
+			<!-- Brand/logo -->
+			
+			<!-- Navbar Items -->
+			
+            <ul class="navbar-nav a ml-auto" id="drop1">
+
+                <li class="nav-item dropdown d-none d-sm-block" id="dropdown_toggle" >
+                    <a class="nav-link dropdown-toggle hover_button" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">Menu
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right " style="text-align: center; overflow:auto;" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item hover_menu_button" href="index.php"><i class="fa fa-home " href="index.php"></i> Home</a>
+                        <div class="dropdown-divider"></div>
+						 <a class="dropdown-item hover_menu_button" href="chat.php"><i class="fa fa-comments-o"  onclick="chat.php"></i> Chat</a>
+						 <div class="dropdown-divider"></div>
+                        <a class="dropdown-item hover_menu_button" href="#"><i class="fa fa-wrench"></i> Support</a>									
+                    </div>
+                </li>
+				<!-- Visible Only On Phone -->	
+				</ul>
+								<!-- Visible Only On Phone -->	
+				</ul>
+				<li class="nav-item dropdown d-block d-sm-none" id="dropdown_toggle" style="z-index:10;">
+                    <a class="nav-link dropdown-toggle hover_button" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">Menu
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right " style="text-align: center; overflow:auto;" aria-labelledby="navbarDropdown">
+
+                        <a class="dropdown-item hover_menu_button " href="index.php"><i class="fa fa-home " href="index.php"></i> Home</a>
+                        <div class="dropdown-divider"></div>
+						 <a class="dropdown-item hover_menu_button" href="chat.php"><i class="fa fa-comments-o"  onclick="index.php"></i> Chat</a>
+						 <div class="dropdown-divider"></div>
+                        <a class="dropdown-item hover_menu_button" href="#"><i class="fa fa-wrench"></i>Support</a>			
+                    </div>
+                </li>
+				<!-- Visible Only On Phone end -->
+        </nav>
+		<div class="container-fluid d-none d-md-block" style="height:128vh; margin-top:4%;overflow:hidden">
+				<div class="row">
+					<div class="col-md-12" style="overflow:hidden">
+							<h1 class="align-middle" style="color:#69E0DC;height:12vh;text-align:center; margin-top:2vh"> Os Grandes <hr style="background-color: white; width:25%; border-radius:50px;"> </h1>
+							<div class="container-fluid" style="height:53vh;">
+							<div class="row">
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:40vh;width:30vw">
+							<p style="color:white;">Danilo Almeida</p>
+							<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> Danilo made us the best design ever..</p>
+							</div>
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:40vh;width:30vw">
+							<p style="color:white">Gabriel Fontes</p>
+								<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> This dude did all the conections to the database, the database itself, a bot and worked in java functions.</p>
+							</div>
+							</div>
+							</div>
+							<div class="container-fluid" style="height:50vh;">
+							<div class="row">
+							<div class="col-md-4 text-center">
+							<img src="Imagens/new_york.jpg" style="height:40vh;width:30vw">
+							<p style="color:white">Inês Moreira</p>
+							<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> This woman did our Android application alone because is coworker didnt want to help her</p>
+							</div>
+							<div class="col-md-4 text-center">
+							<img src="Imagens/new_york.jpg" style="height:40vh;width:30vw">
+							<p style="color:white">André Marques</p>
+								<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> This guy did some java animations and was responsable for the ideas of our site</p>
+							</div>
+							<div class="col-md-4 text-center">
+							<img src="Imagens/new_york.jpg" style="height:40vh;width:30vw">
+							<p style="color:white">Vasco</p>
+								<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> Just Empty</p>
+							</div>
+							</div>
+							</div>
+					</div>
+				</div>
+				<div class="row">
+				<div class="col-md-12 text-center">
+						<hr style="background-color: rgba(1,168,183,1); width:95%; height:0.1vh; border-radius:50px;">
+				<p style="color:white"> O Danilo fez tudo</p>
+				</div>
+				</div>
+			</div>
+			<div class="container-fluid d-sm-none" style="height:231vh; margin-top:4%;overflow:hidden">
+				<div class="row">
+					<div class="col-md-12" style="overflow:hidden">
+							<h1 class="align-middle" style="color:#69E0DC;height:12vh;text-align:center; margin-top:2vh"> Os Grandes <hr style="background-color: white; width:25%; border-radius:50px;"> </h1>
+							<div class="container-fluid" style="overflow:hidden;">
+							<div class="row">
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:25vh">
+							<p style="color:white">Danilo</p>
+							<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> Danilo made us the best design ever, however he is a bit Gay, but we like Him anyway.</p>
+							</div>
+							</div>
+							<div class="row">
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:25vh">
+							<p style="color:white">Gabriel Fontes</p>
+								<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> This dude did all the conections to the database, the database itself, a bot and worked in java functions.</p>
+							</div>
+							</div>
+							<div class="row">
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:25vh">
+							<p style="color:white">Inês Moreira</p>
+							<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> This woman did our Android application alone because is coworker didnt want to help her</p>
+							</div>
+							</div>
+							<div class="row">
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:25vh">
+							<p style="color:white">André Marques</p>
+								<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%"> This guy did some java animations and was responsable for the ideas of our site</p>
+							</div>
+							</div>
+							<div class="row">
+							<div class="col-md-6 text-center">
+							<img src="Imagens/new_york.jpg" style="height:25vh">
+							<p style="color:white">Vasco</p>
+								<p style="font-size:10px; color:white;margin-top:-2%;max-width:50%;margin-left:25%">Just Empty</p>
+							</div>
+							</div>
+							</div>
+					</div>	
+				<div class="row">
+				<div class="col-md-12 text-center">
+						<hr style="background-color: rgba(1,168,183,1); width:100vw; height:0.1vh; border-radius:50px;">
+					  <p style="color:white"> O Danilo fez tudo</p>
+				</div>
+				</div>
+			</div>
+			</div>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
